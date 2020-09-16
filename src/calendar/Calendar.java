db2d24a1bde11ee24883f4ed713934410687270a
LG <LG@DESKTOP-913DI0C>
2020-09-16 20:32:58 +0900

    Maximum number of days of month entered

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,5 +1,5 @@
 package calendar;
-
+import java.util.Scanner;
 public class Calendar {
 
 	public static void main(String[] args) {
@@ -8,7 +8,14 @@ public class Calendar {
 		System.out.println("1  2  3  4  5  6  7");
 		System.out.println("8  9  10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");		
+		System.out.println("22 23 24 25 26 27 28");	
+		
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("달을 입력하세요");
+		int month=scanner.nextInt();
+		int[] maxDays= {31,28,31,30,31,30,31,31,30,31,30,31};
+		System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays[month-1]);
+		scanner.close();
 	}
 
 }
